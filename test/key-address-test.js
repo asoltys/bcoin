f26e0ff3ab737ac1b496a733ff198ac0150b63bd
Adam Soltys <asoltys@gmail.com>
2017-07-09 20:53:09 -0700

    test: litecoinify test addresses

--- test/key-address-test.js
+++ test/key-address-test.js
@@ -1,46 +1,45 @@
 'use strict';
 
-var assert = require('assert');
-var keyring = require('../lib/primitives/keyring');
+const assert = require('assert');
+const keyring = require('../lib/primitives/keyring');
 
 describe('Keyring Address', function() {
-  var ukey = keyring.fromSecret('5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss');
-  var ckey = keyring.fromSecret('L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1');
+  let ukey = keyring.fromSecret('6vrJ6bnKwaSuimkkRLpNNziSjqwZCG59kfFC9P2kjbUUs5Y6Cw9');
+  let ckey = keyring.fromSecret('TAgaTiX4btdMhNY6eSU5N5jvc71o6hXKdhoeBzEk31AHykGDou8i');
 
-  it('check uncompressed public key', function() {
+  it('check uncompressed public key', () => {
     assert.equal(
-      '04a34b99f22c790c4e36b2b3c2c35a36db06226e41c692fc82b8b56ac1c540c5bd5b'
-      + '8dec5235a0fa8722476c7709c02559e3aa73aa03918ba2d492eea75abea235',
+      '04a34b99f22c790c4e36b2b3c2c35a36db06226e41c692fc82b8b56ac1c540c5bd5b8dec5235a0fa8722476c7709c02559e3aa73aa03918ba2d492eea75abea235',
       ukey.getPublicKey('hex'));
   });
 
-  it('check uncompressed public key to address', function() {
+  it('check uncompressed public key to address', () => {
     assert.equal(
-      '1HZwkjkeaoZfTSaJxDw6aKkxp45agDiEzN',
+      'Lbnu1x4UfToiiFGU8MvPrLpj2GSrtUrxFH',
       ukey.getKeyAddress('base58'));
   });
 
-  it('check uncompressed secret', function() {
+  it('check uncompressed secret', () => {
     assert.equal(
-      '5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss',
+      '6vrJ6bnKwaSuimkkRLpNNziSjqwZCG59kfFC9P2kjbUUs5Y6Cw9',
       ukey.toSecret());
   });
 
-  it('check compressed public key', function() {
+  it('check compressed public key', () => {
     assert.equal(
       '03a34b99f22c790c4e36b2b3c2c35a36db06226e41c692fc82b8b56ac1c540c5bd',
       ckey.getPublicKey('hex'));
   });
 
-  it('check compressed public key to address', function() {
+  it('check compressed public key to address', () => {
     assert.equal(
-      '1F3sAm6ZtwLAUnj7d38pGFxtP3RVEvtsbV',
+      'LZGpRyQPybaDjbRGoB87YH2ebFnmKYmRui',
       ckey.getKeyAddress('base58'));
   });
 
-  it('check compressed secret', function() {
+  it('check compressed secret', () => {
     assert.equal(
-      'L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1',
+      'TAgaTiX4btdMhNY6eSU5N5jvc71o6hXKdhoeBzEk31AHykGDou8i',
       ckey.toSecret());
   });
 });
